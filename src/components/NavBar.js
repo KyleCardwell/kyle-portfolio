4cd985cb543d98c0167067d3a60bd9ccb185dc04
Kyle <kyle.e.cardwell@gmail.com>
2021-11-14 21:46:49 -0700

    empty navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NavBar = () => {
+
+    return(
+        <nav className="bg-gray-800">
+            <h2>HELLO</h2>
+
+        </nav>
+    )
+};
+
+export default NavBar;
